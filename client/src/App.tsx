143f247be8c9b84a5988fd041d1e7a67f50120bb
Tapiwa Mahachi <mahachitapiwa13@gmail.com>
2021-07-11 19:39:40 +0200

    :rocket: Linting support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import Header from './components/Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import Admin from './components/Admin';
 import './css/style.css';
 import Login from './components/Login';
 
-
-
-function App() {
-  
+function App():React.ReactElement {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Switch>
         <Route path="/admin/:id?">
           <Admin />
         </Route>
         <Route path="/">
-          <Login/>
+          <Login />
         </Route>
-      </Switch>  
+      </Switch>
     </Router>
   );
 }
---
+++ client/src/layouts/Admin.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Admin():React.ReactElement {
+  return (
+    <div />
+  );
+}
+
+export default Admin;
